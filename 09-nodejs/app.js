15986860c0eaaab692d288646ee315b0ab05ecaa
venguyen <phuongve.nina@gmail.com>
2026-05-31 15:18:48 -0700

    labnote9 nodejs commit

--- 09-nodejs/app.js
+++ 09-nodejs/app.js
@@ -1,34 +1,40 @@
-const http = require('http');
-const static = require('node-static');
-const querystring = require('node:querystring');
+const http = require("http");
+const static = require("node-static");
+const querystring = require("node:querystring");
 
 const port = process.env.PORT || 5002;
 
-const file = new static.Server('./exercise');
+const file = new static.Server("./exercise");
 
 const server = http.createServer((req, res) => {
   // main route
-  if (req.method === 'GET' && req.url === '/') {
-    file.serveFile('/welcome.html', 200, {}, req, res);
+  if (req.method === "GET" && req.url === "/") {
+    file.serveFile("/welcome.html", 200, {}, req, res);
   }
-  // form route
-  else if (req.method === 'GET' && req.url === '') {
-    // fill out this route
-  }
-  // form submission
-  else if (req.method === 'POST' && req.url === '') {
-    let body = '';
 
-    req.on('data', (chunk) => {
+  // form route
+  else if (req.method === "GET" && req.url === "/form") {
+    file.serveFile("/form.html", 200, {}, req, res);
+  }
+
+  // form submission
+  else if (req.method === "POST" && req.url === "/formExerciseSubmit") {
+    let body = "";
+
+    req.on("data", (chunk) => {
       body += chunk;
     });
 
-    req.on('end', () => {
+    req.on("end", () => {
       const userdata = querystring.parse(body);
-      const { usernameInput: name, emailInput: email } = userdata;
 
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(`<p>Thank you for submitting your information: </p>`);
+      const { nameInput: name, emailInput: email } = userdata;
+
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      res.write("<p>Thank you for submitting your information:</p>");
       res.write(`<p>Name: ${name}</p>`);
       res.write(`<p>Email: ${email}</p>`);
       res.end();
